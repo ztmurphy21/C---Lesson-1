103ac85654ee61cfc679155318910c99df62bc5d
Zachary Murphy <ztmurphy21@gmail.com>
2017-09-24 11:25:54 -0400

    initial

---
+++ Lesson 1/Lesson 1/Lesson 1.cpp
@@ -0,0 +1,17 @@
+// Lesson 1.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+
+int main()
+{
+	cout << "Progrmming is awesome!" <<endl;
+	cout << "Hello my name is Zach!\n";
+	cout << "Press any key to exit...";
+	_getch();
+    return 0;
+}
+
